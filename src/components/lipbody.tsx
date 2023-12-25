4b5ae9928b8291dd6b713bae9beb4b3b03cae769
anshu <lcb2021043@iiitl.ac.in>
2023-12-25 10:12:01 +0530

    fix: iframe video component

--- src/components/lipbody.tsx
+++ src/components/lipbody.tsx
@@ -66,10 +66,10 @@ const Lipbody =() => {
                 console.log("backend response",response)
 
             }
-            // setLoading(false)
-            // console.log("this ran after res")
-            // setVideoPath(`${HOST}/static/${response!.videoPath}`)
-            // console.log("this is full url ",videoPath )
+             setLoading(false)
+             console.log("this ran after res")
+             setVideoPath(`${HOST}/static/${response!.videoPath}`)
+             console.log("this is full url ",videoPath )
             
 
     
@@ -137,7 +137,7 @@ const Lipbody =() => {
 <div className='flex justify-center mt-4 ml-6'>
 <button onClick={handlefile} className="relative flex ml-30   justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
 <span className="px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-Submit
+{loading ? "Loading..." : "Generate" }
 </span>
 </button>
 
